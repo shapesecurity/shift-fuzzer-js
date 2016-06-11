1814e221dcbb4926875718a8be0c4c9ad4ab9a4f
Kevin Gibbons <bakkot@gmail.com>
2016-06-10 21:35:43 -0700

    hex digits in regexs generate properly (#4)

--- src/regexp.js
+++ src/regexp.js
@@ -69,7 +69,7 @@ const fuzzPrintableAscii = f => {
   return String.fromCharCode(32 + f.rng.nextInt(94));
 }
 
-const fuzzHex = oneOf(['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','A','B','C','D','E','F']);
+const fuzzHex = oneOf('0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','A','B','C','D','E','F');
 
 
 const fuzzAlternation = f => {
