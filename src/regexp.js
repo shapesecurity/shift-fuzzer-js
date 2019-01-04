1c9e3d88f349115f7c2392dacf9a18a37db4bba8
mbruce <max.bruce12@gmail.com>
2019-01-04 12:51:23 -0800

    remove es2018 features

--- src/regexp.js
+++ src/regexp.js
@@ -11,24 +11,19 @@ function testRegex(regex, flags = '', constructor = RegExp) {
 }
 
 export class RegExpBugAvoidanceConfiguration {
-  constructor({namedGroups = true, lookbehinds = true, unicodeProperties = true, loneUnicodePropertiesBroken = [], loneUnicodeProperties = utf16LonePropertyValuesRaw.filter(value => loneUnicodePropertiesBroken.indexOf(value) === -1)} = {}) {
-    this.namedGroups = namedGroups;
-    this.unicodeProperties = unicodeProperties;
-    this.loneUnicodePropertiesBroken = loneUnicodePropertiesBroken;
-    this.loneUnicodeProperties = loneUnicodeProperties;
+  constructor({ lookbehinds = true }) {
     this.lookbehinds = lookbehinds;
   }
 
   static fromEngine(constructor = RegExp) {
-    return new RegExpBugAvoidanceConfiguration({namedGroups: !!testRegex('(?<t>)', '', constructor), lookbehinds: !!testRegex('(?<=)(?<!)', '', constructor), unicodeProperties: !!testRegex('\\p{ASCII}', 'u', constructor), loneUnicodePropertiesBroken: defaultBrokenLoneUnicodeValues(constructor)});
+    return new RegExpBugAvoidanceConfiguration({ lookbehinds: !!testRegex('(?<=)(?<!)', '', constructor) });
   }
 }
 
 class RegExpGlobalState {
   constructor() {
-    this.definedGroupSpecifiers = [];
-    this.groupSpecifiersToDefine = [];
     this.noNumericLookahead = false;
+    this.groupSpecifiersToDefine = 0;
   }
 }
 
@@ -128,58 +123,10 @@ const controlLetters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.sp
 
 const getSyntaxCharacters = state => state.unicode ? unicodeSyntaxCharacters : syntaxCharacters;
 
-const utf16GeneralCategoryValues = ['Cased_Letter', 'LC', 'Close_Punctuation', 'Pe', 'Connector_Punctuation', 'Pc', 'Control', 'Cc', 'cntrl', 'Currency_Symbol', 'Sc', 'Dash_Punctuation', 'Pd', 'Decimal_Number', 'Nd', 'digit', 'Enclosing_Mark', 'Me', 'Final_Punctuation', 'Pf', 'Format', 'Cf', 'Initial_Punctuation', 'Pi', 'Letter', 'L', 'Letter_Number', 'Nl', 'Line_Separator', 'Zl', 'Lowercase_Letter', 'Ll', 'Mark', 'M', 'Combining_Mark', 'Math_Symbol', 'Sm', 'Modifier_Letter', 'Lm', 'Modifier_Symbol', 'Sk', 'Nonspacing_Mark', 'Mn', 'Number', 'N', 'Open_Punctuation', 'Ps', 'Other', 'C', 'Other_Letter', 'Lo', 'Other_Number', 'No', 'Other_Punctuation', 'Po', 'Other_Symbol', 'So', 'Paragraph_Separator', 'Zp', 'Private_Use', 'Co', 'Punctuation', 'P', 'punct', 'Separator', 'Z', 'Space_Separator', 'Zs', 'Spacing_Mark', 'Mc', 'Surrogate', 'Cs', 'Symbol', 'S', 'Titlecase_Letter', 'Lt', 'Unassigned', 'Cn', 'Uppercase_Letter', 'Lu']
-
-const utf16ScriptCategoryValues = ['Adlam', 'Adlm', 'Ahom', 'Anatolian_Hieroglyphs', 'Hluw', 'Arabic', 'Arab', 'Armenian', 'Armn', 'Avestan', 'Avst', 'Balinese', 'Bali', 'Bamum', 'Bamu', 'Bassa_Vah', 'Bass', 'Batak', 'Batk', 'Bengali', 'Beng', 'Bhaiksuki', 'Bhks', 'Bopomofo', 'Bopo', 'Brahmi', 'Brah', 'Braille', 'Brai', 'Buginese', 'Bugi', 'Buhid', 'Buhd', 'Canadian_Aboriginal', 'Cans', 'Carian', 'Cari', 'Caucasian_Albanian', 'Aghb', 'Chakma', 'Cakm', 'Cham', 'Cherokee', 'Cher', 'Common', 'Zyyy', 'Coptic', 'Copt', 'Qaac', 'Cuneiform', 'Xsux', 'Cypriot', 'Cprt', 'Cyrillic', 'Cyrl', 'Deseret', 'Dsrt', 'Devanagari', 'Deva', 'Dogra', 'Dogr', 'Duployan', 'Dupl', 'Egyptian_Hieroglyphs', 'Egyp', 'Elbasan', 'Elba', 'Ethiopic', 'Ethi', 'Georgian', 'Geor', 'Glagolitic', 'Glag', 'Gothic', 'Goth', 'Grantha', 'Gran', 'Greek', 'Grek', 'Gujarati', 'Gujr', 'Gunjala_Gondi', 'Gong', 'Gurmukhi', 'Guru', 'Han', 'Hani', 'Hangul', 'Hang', 'Hanifi_Rohingya', 'Rohg', 'Hanunoo', 'Hano', 'Hatran', 'Hatr', 'Hebrew', 'Hebr', 'Hiragana', 'Hira', 'Imperial_Aramaic', 'Armi', 'Inherited', 'Zinh', 'Qaai', 'Inscriptional_Pahlavi', 'Phli', 'Inscriptional_Parthian', 'Prti', 'Javanese', 'Java', 'Kaithi', 'Kthi', 'Kannada', 'Knda', 'Katakana', 'Kana', 'Kayah_Li', 'Kali', 'Kharoshthi', 'Khar', 'Khmer', 'Khmr', 'Khojki', 'Khoj', 'Khudawadi', 'Sind', 'Lao', 'Laoo', 'Latin', 'Latn', 'Lepcha', 'Lepc', 'Limbu', 'Limb', 'Linear_A', 'Lina', 'Linear_B', 'Linb', 'Lisu', 'Lycian', 'Lyci', 'Lydian', 'Lydi', 'Mahajani', 'Mahj', 'Makasar', 'Maka', 'Malayalam', 'Mlym', 'Mandaic', 'Mand', 'Manichaean', 'Mani', 'Marchen', 'Marc', 'Medefaidrin', 'Medf', 'Masaram_Gondi', 'Gonm', 'Meetei_Mayek', 'Mtei', 'Mende_Kikakui', 'Mend', 'Meroitic_Cursive', 'Merc', 'Meroitic_Hieroglyphs', 'Mero', 'Miao', 'Plrd', 'Modi', 'Mongolian', 'Mong', 'Mro', 'Mroo', 'Multani', 'Mult', 'Myanmar', 'Mymr', 'Nabataean', 'Nbat', 'New_Tai_Lue', 'Talu', 'Newa', 'Nko', 'Nkoo', 'Nushu', 'Nshu', 'Ogham', 'Ogam', 'Ol_Chiki', 'Olck', 'Old_Hungarian', 'Hung', 'Old_Italic', 'Ital', 'Old_North_Arabian', 'Narb', 'Old_Permic', 'Perm', 'Old_Persian', 'Xpeo', 'Old_Sogdian', 'Sogo', 'Old_South_Arabian', 'Sarb', 'Old_Turkic', 'Orkh', 'Oriya', 'Orya', 'Osage', 'Osge', 'Osmanya', 'Osma', 'Pahawh_Hmong', 'Hmng', 'Palmyrene', 'Palm', 'Pau_Cin_Hau', 'Pauc', 'Phags_Pa', 'Phag', 'Phoenician', 'Phnx', 'Psalter_Pahlavi', 'Phlp', 'Rejang', 'Rjng', 'Runic', 'Runr', 'Samaritan', 'Samr', 'Saurashtra', 'Saur', 'Sharada', 'Shrd', 'Shavian', 'Shaw', 'Siddham', 'Sidd', 'SignWriting', 'Sgnw', 'Sinhala', 'Sinh', 'Sogdian', 'Sogd', 'Sora_Sompeng', 'Sora', 'Soyombo', 'Soyo', 'Sundanese', 'Sund', 'Syloti_Nagri', 'Sylo', 'Syriac', 'Syrc', 'Tagalog', 'Tglg', 'Tagbanwa', 'Tagb', 'Tai_Le', 'Tale', 'Tai_Tham', 'Lana', 'Tai_Viet', 'Tavt', 'Takri', 'Takr', 'Tamil', 'Taml', 'Tangut', 'Tang', 'Telugu', 'Telu', 'Thaana', 'Thaa', 'Thai', 'Tibetan', 'Tibt', 'Tifinagh', 'Tfng', 'Tirhuta', 'Tirh', 'Ugaritic', 'Ugar', 'Vai', 'Vaii', 'Warang_Citi', 'Wara', 'Yi', 'Yiii', 'Zanabazar_Square', 'Zanb'];
-
-const utf16LonePropertyValuesRaw = ['ASCII', 'ASCII_Hex_Digit', 'AHex', 'Alphabetic', 'Alpha', 'Any', 'Assigned', 'Bidi_Control', 'Bidi_C', 'Bidi_Mirrored', 'Bidi_M', 'Case_Ignorable', 'CI', 'Cased', 'Changes_When_Casefolded', 'CWCF', 'Changes_When_Casemapped', 'CWCM', 'Changes_When_Lowercased', 'CWL', 'Changes_When_NFKC_Casefolded', 'CWKCF', 'Changes_When_Titlecased', 'CWT', 'Changes_When_Uppercased', 'CWU', 'Dash', 'Default_Ignorable_Code_Point', 'DI', 'Deprecated', 'Dep', 'Diacritic', 'Dia', 'Emoji', 'Emoji_Component', 'Emoji_Modifier', 'Emoji_Modifier_Base', 'Emoji_Presentation', 'Extended_Pictographic', 'Extender', 'Ext', 'Grapheme_Base', 'Gr_Base', 'Grapheme_Extend', 'Gr_Ext', 'Hex_Digit', 'Hex', 'IDS_Binary_Operator', 'IDSB', 'IDS_Trinary_Operator', 'IDST', 'ID_Continue', 'IDC', 'ID_Start', 'IDS', 'Ideographic', 'Ideo', 'Join_Control', 'Join_C', 'Logical_Order_Exception', 'LOE', 'Lowercase', 'Lower', 'Math', 'Noncharacter_Code_Point', 'NChar', 'Pattern_Syntax', 'Pat_Syn', 'Pattern_White_Space', 'Pat_WS', 'Quotation_Mark', 'QMark', 'Radical', 'Regional_Indicator', 'RI', 'Sentence_Terminal', 'STerm', 'Soft_Dotted', 'SD', 'Terminal_Punctuation', 'Term', 'Unified_Ideograph', 'UIdeo', 'Uppercase', 'Upper', 'Variation_Selector', 'VS', 'White_Space', 'space', 'XID_Continue', 'XIDC', 'XID_Start', 'XIDS']
-    .concat(utf16GeneralCategoryValues);
-
-
-//NOTE: this is *really* slow, as you can imagine. the only value that I have found to fail is Extended_Pictographic, and we may want to hardcore it.
-let defaultBrokenLoneUnicodeValuesCached;
-let defaultBrokenLoneUnicodeValuesCachedConstructor;
-
-const defaultBrokenLoneUnicodeValuesCalculate = () => !testRegex('\\p{ASCII}', 'u') ? [] : utf16LonePropertyValuesRaw.filter(value => !testRegex(`/\\p{${value}}`, 'u'));
-
-const defaultBrokenLoneUnicodeValues = (constructor) => {
-  if (defaultBrokenLoneUnicodeValuesCachedConstructor !== constructor) {
-    defaultBrokenLoneUnicodeValuesCachedConstructor = constructor;
-    defaultBrokenLoneUnicodeValuesCached = void 0;
-  }
-  return defaultBrokenLoneUnicodeValuesCached || (defaultBrokenLoneUnicodeValuesCached = defaultBrokenLoneUnicodeValuesCalculate())
-};
-
-const fuzzLoneUnicodePropertyNameOrValue = f => f.bugAvoidance.loneUnicodeProperties[f.rng.nextInt(f.bugAvoidance.loneUnicodeProperties.length)];
-
-const utf16NonBinaryPropertyNames = {
-  General_Category: utf16GeneralCategoryValues,
-  gc: utf16GeneralCategoryValues,
-  Script: utf16ScriptCategoryValues,
-  sc: utf16ScriptCategoryValues,
-  Script_Extensions: utf16ScriptCategoryValues,
-  scx: utf16ScriptCategoryValues,
-};
-
-const fuzzUnicodePropertyNameAndValue = f => {
-  let propertyName = oneOf(...Object.keys(utf16NonBinaryPropertyNames))(f);
-  let possibleValues = utf16NonBinaryPropertyNames[propertyName]
-  let value = possibleValues[f.rng.nextInt(possibleValues.length)];
-  return `${propertyName}=${value}`;
-}
-
-const fuzzUnicodePropertyValueExpression = choose(fuzzUnicodePropertyNameAndValue, fuzzLoneUnicodePropertyNameOrValue);
-
 const fuzzUnicodeEscape = onlyUnicode => state => {
   let unicodeFuzzers = [];
   if (state.unicode) {
     let firstFuzzedHex = null;
-    if (!onlyUnicode && state.bugAvoidance.unicodeProperties) {
-      unicodeFuzzers.push(
-        f => `\\${oneOf('p', 'P')(f)}{${fuzzUnicodePropertyValueExpression(f)}}`, // unicode character class escape
-        f => `\\${oneOf(...syntaxCharacters, '/')(f)}`, // syntax characters, including .
-      );
-    }
     unicodeFuzzers.push(
       f => `\\u{${firstFuzzedHex = fuzzHex(f)}${firstFuzzedHex === 'd' || firstFuzzedHex === 'D' ? fuzzHexExcept(f, '89abcdefABCDEF'.split('')) : fuzzHex(f)}${fuzzHex(f)}${fuzzHex(f)}}`,
       f => `\\u${firstFuzzedHex = fuzzHex(f)}${firstFuzzedHex === 'd' || firstFuzzedHex === 'D' ? fuzzHexExcept(f, '89abcdefABCDEF'.split('')) : fuzzHex(f)}${fuzzHex(f)}${fuzzHex(f)}`,
@@ -197,15 +144,13 @@ const fuzzUnicodeEscape = onlyUnicode => state => {
 
 const octal = decimal.slice(0, 8);
 
-const occupiedEscapes = ['d', 'D', 's', 'S', 'w', 'W', 'p', 'P', 'f', 'n', 'r', 't', 'v', 'u', 'x', 'b', 'B', 'c', 'k', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+const occupiedEscapes = ['d', 'D', 's', 'S', 'w', 'W', 'f', 'n', 'r', 't', 'v', 'u', 'x', 'b', 'B', 'c', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
 const fuzzDecimalEscape = state => {
-  let nParens = state.globalState.groupSpecifiersToDefine.length + state.globalState.definedGroupSpecifiers.length;
-  if (nParens === 0) {
-    state.globalState.groupSpecifiersToDefine.push(fuzzRegexIdentifierWithValue(state));
-    nParens++;
+  if (state.globalState.groupSpecifiersToDefine === 0) {
+    ++state.globalState.groupSpecifiersToDefine;
   }
-  return `\\${state.rng.nextInt(nParens) + 1}`
+  return `\\${state.rng.nextInt(state.globalState.groupSpecifiersToDefine) + 1}`
 }
 
 const fuzzCharacterEscapes = state => {
@@ -266,88 +211,6 @@ const encodeSurrogatePair = codePoint => {
   return [0xD800 | (codePoint >> 10), 0xDC00 | (codePoint & 0x3FF)];
 }
 
-const fuzzCodePoint = characters => f => {
-  let codePoint = characters[f.rng.nextInt(characters.length)].codePointAt(0);
-  let encoders = [
-    () => {
-      if (codePoint < 0x10000) {
-        return `\\u${padHex(codePoint.toString(16), 4)}`
-      } else {
-        let pair = encodeSurrogatePair(codePoint);
-        return pair.map(point => `\\u${padHex(point.toString(16), 4)}`).join('');
-      }
-    },
-  ];
-  if (f.unicode) {
-    encoders.push(() => `\\u{${codePoint.toString(16)}}`);
-  }
-  return choose(...encoders)();
-};
-
-const IDENTIFIER_START_ASCII = IDENTIFIER_START.filter(v => v.codePointAt(0) < 128);
-const IDENTIFIER_CONTINUE_ASCII = IDENTIFIER_CONTINUE.filter(v => v.codePointAt(0) < 128);
-
-const fuzzIdentifierContinue = f => fuzzCharacters(f.unicode ? IDENTIFIER_CONTINUE : IDENTIFIER_CONTINUE_ASCII)(f);
-
-const fuzzCodePointIdentifierContinue = f => fuzzCodePoint(f.unicode ? IDENTIFIER_CONTINUE : IDENTIFIER_CONTINUE_ASCII)(f);
-
-const fuzzIdentifierStart = f => fuzzCharacters(f.unicode ? IDENTIFIER_START : IDENTIFIER_START_ASCII)(f);
-
-const fuzzCodePointIdentifierStart = f => fuzzCodePoint(f.unicode ? IDENTIFIER_START : IDENTIFIER_START_ASCII)(f);
-
-const fuzzSpecialIdentifierContinueCharacter = f => oneOf('\u200C', '\u200D')(f); // ZWNJ / ZWJ
-
-const fuzzRegexIdentifier = state => {
-  return `${choose(f => oneOf('$', '_')(f), fuzzCodePointIdentifierStart, fuzzIdentifierStart)(state)}${many(choose(f => '$', fuzzCodePointIdentifierContinue, fuzzIdentifierContinue, fuzzSpecialIdentifierContinueCharacter))(state).join('')}`;
-}
-
-const fuzzRegexIdentifierWithValue = state => {
-  let identifier;
-  let value;
-  do {
-    identifier = fuzzRegexIdentifier(state);
-    let identifierFiltered = identifier.replace(/\\u\{[0-9a-fA-F]+\}/g, (matched) => encodeSurrogatePair(parseInt(matched.slice(3), 16)).map(num => `\\u${padHex(num.toString(16), 4)}`).join(''));
-    value = JSON.parse(`"${identifierFiltered}"`);
-  } while(state.globalState.definedGroupSpecifiers.find(obj => obj.value === value) || state.globalState.groupSpecifiersToDefine.find(obj => obj.value === value));
-  return {identifier, value};
-}
-
-const fuzzRegexIdentifierDeclaration = state => {
-  let identifier
-  let value;
-  if (state.globalState.groupSpecifiersToDefine.length > 0) {
-    ({identifier, value} = state.globalState.groupSpecifiersToDefine.pop());
-  } else {
-    ({identifier, value} = fuzzRegexIdentifierWithValue(state));
-  }
-  state.globalState.definedGroupSpecifiers.push({identifier, value});
-  return identifier;
-}
-
-const fuzzRegexIdentifierReference = state => {
-  if (state.globalState.definedGroupSpecifiers.length === 0) {
-    if (state.globalState.groupSpecifiersToDefine.length === 0) {
-      state.globalState.groupSpecifiersToDefine.push(fuzzRegexIdentifierWithValue(state));
-    }
-    return state.globalState.groupSpecifiersToDefine[state.rng.nextInt(state.globalState.groupSpecifiersToDefine.length)].identifier;
-  }
-  return state.globalState.definedGroupSpecifiers[state.rng.nextInt(state.globalState.definedGroupSpecifiers.length)].identifier;
-}
-
-const fuzzGroupName = (f, isDeclaration) => `<${isDeclaration ? fuzzRegexIdentifierDeclaration(f) : fuzzRegexIdentifierReference(f)}>`
-
-const fuzzGroupSpecifier = (f, isDeclaration) => !f.bugAvoidance.namedGroups ? '' : choose(f => '', f => `?${fuzzGroupName(f, isDeclaration)}`)(f);
-
-const fuzzGrouping = f => {
-  if (f.tooDeep()) return '()';
-  f = f.goDeeper();
-  f.globalState.noNumericLookahead = false;
-  let spec = fuzzGroupSpecifier(f, true);
-  let value = `(${spec}${fuzzDisjunction(f)})`;
-  f.globalState.noNumericLookahead = false;
-  return value;
-}
-
 const fuzzNoCaptureGrouping = f => {
   if (f.tooDeep()) return '()';
   f = f.goDeeper();
@@ -399,13 +262,9 @@ const fuzzAtom = state => {
     fuzzPatternCharacter,
     fuzzCharacterEscapes,
     fuzzCharacterClass,
-    fuzzGrouping,
     fuzzNoCaptureGrouping,
     // invalid braced quantifier intentionally never generated ... it is always an error
   ];
-  if (state.bugAvoidance.namedGroups) {
-    characterFuzzers.push(f => `\\k${fuzzGroupName(f, false)}`);
-  }
   if (state.unicode) {
     characterFuzzers.push(fuzzDecimalEscape);
   }
@@ -463,8 +322,8 @@ const fuzzCharacterClass = f => {
   return `[${oneOf('^', '')(f)}${source}${oneOf('-', '')(f)}]`;
 };
 
-const illegalRangeItems = ['\\c', '\\k'];
-const illegalRangeItemsUnicode = ['\\d', '\\D', '\\w', '\\W', '\\s', '\\S', '\\c', '\\k', '\\p', '\\P'];
+const illegalRangeItems = ['\\c'];
+const illegalRangeItemsUnicode = ['\\d', '\\D', '\\w', '\\W', '\\s', '\\S', '\\c', '\\p', '\\P'];
 
 const fuzzCharacterClassRange = canDash => f => {
   let toCheckItems = illegalRangeItems;
@@ -544,12 +403,8 @@ export default function fuzzRegExpPattern(f = {rng: new Random(Math.random)}, un
   let state = new RegExpState({rng: f.rng, unicode: unicode, bugAvoidance: bugAvoidance});
   let rv = fuzzDisjunction(state);
   while (state.globalState.groupSpecifiersToDefine.length > 0) {
-    if (state.bugAvoidance.namedGroups) {
-      rv += `(?<${state.globalState.groupSpecifiersToDefine.pop().identifier}>)`;
-    } else {
-      state.globalState.groupSpecifiersToDefine.pop();
-      rv += `()`;
-    }
+    state.globalState.groupSpecifiersToDefine.pop();
+    rv += `()`;
   }
   if (rv === '') return '(?:)';
   return rv;
